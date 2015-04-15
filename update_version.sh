f8b214157183df1d715417ae0f73ba23ac8aa997
areski <areski@gmail.com>
2015-04-15 18:52:54 +0200

    remove gitflow from update_version script

--- update_version.sh
+++ update_version.sh
@@ -3,7 +3,6 @@
 #   ./update_version.sh 0.2.3
 #
 
-git flow release start v$1
 sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" acapela.py
 sed -i -e "s/version = '.*'/version = '$1'/g" docs/source/conf.py
 #rm -rf docs/html
@@ -11,5 +10,5 @@ sed -i -e "s/version = '.*'/version = '$1'/g" docs/source/conf.py
 #make docs
 #git commit docs admin_tools_stats/__init__.py -m "Update to version v$1"
 git commit -a -m "Update to version v$1"
-git flow release finish v$1
+git push
 python setup.py sdist upload -r pypi
